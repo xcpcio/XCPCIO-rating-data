d78a146520404199213f1f1235958a193ef3a96c
Dup4 <panlvzhi@foxmail.com>
2021-01-17 23:24:03 +0800

    feat: add list

---
+++ get_info.py
@@ -0,0 +1,42 @@
+import json
+import sys
+import math
+import logging
+import os
+from shutil import copyfile, rmtree, make_archive
+from time import strftime, localtime, time
+import argparse
+
+
+def ensure_dir(s):
+    if not os.path.exists(s):
+        os.makedirs(s)
+
+def ensure_no_dir(s):
+    if os.path.exists(s):
+        rmtree(s)
+
+def json_output(data):
+    return json.dumps(data, sort_keys=False, indent=4, separators=(',', ':'), ensure_ascii=False)
+
+def output(filename, data):
+    with open(filename, 'w') as f:
+        f.write(json_output(data))
+
+def json_input(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+parser = argparse.ArgumentParser(description='Get Season Info.')
+parser.add_argument('-i', '--input', type=str, help='path of input file')
+parser.add_argument('-o', '--output', type=str, help='path of output file')
+parser.add_argument('-d', '--dir', type=str, help='path of season dir')
+args = parser.parse_args()
+
+config = json_input(args.input)
+data = json_input(args.output)
+data.append({
+    'name': config['name'],
+    'link': args.dir
+})
+output(args.output, data)
--- main.sh
+++ main.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+set -x
+
+all_dir_list=(
+    'CNXCPC_2020-2021'
+)
+
 dir_list=(
     'CNXCPC_2020-2021'
 )
 
-set -x
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 mkdir site
 
@@ -16,3 +22,11 @@ do
     cp ./$dir/rating.json site/$dir/rating.json
     cp ./$dir/config.json site/$dir/config.json
 done
+
+[ ! -f list.json ] && echo [] > list.json
+for dir in ${all_dir_list[@]}
+do
+    python3 get_info.py -i=$SHELL_FOLDER/$dir/config.json -o=$SHELL_FOLDER/list.json -d=$dir
+done
+
+cp list.json site/list.json
